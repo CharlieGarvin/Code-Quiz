d1adb1afd11b31ef107bbe4883b26ac401c099e7
unknown <charliegarvin90@gmail.com>
2023-01-12 13:42:54 -0700

    added lots of new functions to get the answers of my questions to show up. Need to debug why it won't progress through the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,33 +8,44 @@ var timeEl = document.getElementById('timer');
 var timeLeft = 60;
 
 var question1 = {
-    text: "this is question 1", 
-    choices: ["a", "b", "c", "d"],
-    answer: "a"
+    text: "Inside which HTMl element do we put the JavaScript?", 
+    choices: ['<js>', '<script>', '<javascript>', '<scripting>'],
+    answer: "a", correct: false,
+    answer: "b", correct: true,
+    answer: "c", correct: false,
+    answer: "d", correct: false
 }
 
 var question2 = {
-    text: "this is question 2", 
-    choices: ["a", "b", "c", "d"],
-    answer: "a"
+    text: "Where is the correct place to insert JavaScript?", 
+    choices: ['the <body> section', 'Both the <head> section and the <body> section are correct', 'The <head> section', "it doesn't matter"],
+    answer: "a", correct: true,
+    answer: "b", correct: false,
+    answer: "c", correct: false,
+    answer: "d", correct: false
 }
 var question3 = {
-    text: "this is question 3", 
-    choices: ["a", "b", "c", "d"],
-    answer: "a"
+    text: "The external JavaScript file must contain the <script> tag", 
+    choices: ['true', 'false'],
+    answer: "a", correct: true,
+    answer: "b", correct: false,
 }
 var question4 = {
-    text: "this is question 4", 
-    choices: ["a", "b", "c", "d"],
-    answer: "a"
+    text: "How do you create a function in JavaScript?", 
+    choices: ['function myFunction()', 'function:myFunction()', 'function = myFunction()', 'function Myfunction()'],
+    answer: "a", correct: true,
+    answer: "b", correct: false,
+    answer: "c", correct: false,
+    answer: "d", correct: false
 }
 var question5 = {
-    text: "this is question 5", 
-    choices: ["a", "b", "c", "d"],
-    answer: "a"
+    text: "How does a while loop start?", 
+    choices: ['while (i <= 10; i++)', 'while i = 1 to 10', 'while (i <= 10)'],
+    answer: "a", correct: true,
+    answer: "b", correct: false,
+    answer: "c", correct: false
 }
 var quizQuestion = [question1, question2, question3, question4, question5];
-console.log(quizQuestion);
 
 start.addEventListener("click", startQuiz);
 
@@ -45,7 +56,6 @@ function startQuiz() {
     for (let i = 0; i < choiceBtn.length; i++) {
         choiceBtn[i].textContent = quizQuestion[questionIndex].choices[i];  
     }
-    timer();
 }
 
 var timerInterval = setInterval( function timer() {
@@ -57,10 +67,56 @@ var timerInterval = setInterval( function timer() {
     }
 }, 1000);
 
+
+
 function nextQuestion() {
-
+    resetQuestion()
+    displayQuestion(questionIndex) 
 }
 
-function chosenAnswer() {
-    
+function displayQuestion(quizQuestion) {
+    questions.innerText = quizQuestion;
+    questions.answer.forEach(answer => {
+        var button = document.createElement('button')
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', chosenAnswer);
+        choiceBtn.appendChild(button)
+    })
 }
+
+function resetQuestion() {
+    choiceBtn.classList.add('hide')
+    while (choiceBtn.firstChild) {
+        choiceBtn.removeChild
+        (choiceBtn.firstChild)
+    }
+}
+
+function chosenAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    statusClass(document.body, correct)
+    choiceBtn.classList.remove('hide')
+}
+
+function statusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass (element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+choiceBtn.addEventListener('click', () => {
+    questionIndex++
+    nextQuestion()
+})
